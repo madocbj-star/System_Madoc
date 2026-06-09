094d7f1a39e9f365b871a683e6a833d53c15f0c7
Julian <julian99m.jj@gmail.com>
2026-06-09 16:48:50 -0500

    Cambiar correo a SSL puerto 465

--- config.py
+++ config.py
@@ -38,9 +38,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # CONFIGURACIÓN MAIL
 # =========================================
 
-MAIL_SERVER = os.environ.get('MAIL_SERVER')
-MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
-MAIL_USE_TLS = True
+MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+MAIL_PORT = int(os.environ.get('MAIL_PORT', 465))
+MAIL_USE_TLS = False
+MAIL_USE_SSL = True
 MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
 MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
